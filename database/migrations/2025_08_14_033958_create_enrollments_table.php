f68aa4e8a52e9a4ca9825cf2f844cb8349fc7b54
Lenovo <orvinoray@gmail.com>
2025-10-03 08:51:15 +0700

    penghapusan migrasi forum

--- database/migrations/2025_08_14_033958_create_enrollments_table.php
+++ database/migrations/2025_08_14_033958_create_enrollments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('course_id')->constrained('course')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
             $table->enum('status', ['active', 'completed', 'cancelled'])->default('active');
             $table->timestamps();
         });
