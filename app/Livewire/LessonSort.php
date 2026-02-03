213da6b561d9f3d29f81ed9da564fb14fb1900dd
Lenovo <orvinoray@gmail.com>
2026-02-03 23:14:45 +0700

    menambahkan fitur pengurutan pelajaran dengan Livewire

--- app/Livewire/LessonSort.php
+++ app/Livewire/LessonSort.php
@@ -7,6 +7,20 @@ use App\Models\Lessons;
 
 class LessonSort extends Component
 {
+
+    public $lessons;
+
+    public function mount()
+    {
+        $this->lessons = Lessons::orderBy('order')->get();
+    }
+
+    public function updateLessonOrder($orders)
+    {
+        foreach ($orders as $order) {
+            Lessons::where('id', $order['value'])->update(['order' => $order['order']]);
+        }
+    }
     public function render()
     {
         return view('livewire.lesson-sort');
