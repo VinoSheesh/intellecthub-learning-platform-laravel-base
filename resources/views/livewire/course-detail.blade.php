4fed308f311e40bf7bffbf079f91b2e472012a1c
Lenovo <orvinoray@gmail.com>
2026-03-13 19:33:28 +0700

    perbaikan routing breadcrumbs pada page course detail

--- resources/views/livewire/course-detail.blade.php
+++ resources/views/livewire/course-detail.blade.php
@@ -10,14 +10,14 @@
     <nav class="flex mb-8" aria-label="Breadcrumb">
         <ol class="inline-flex items-center space-x-1 md:space-x-3">
             <li class="inline-flex items-center">
-                <a href="{{ route('dashboard') }}"
+                <a href="{{ route('admindashboard') }}"
                     class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 transition-colors">
                     <svg class="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                         fill="currentColor" viewBox="0 0 20 20">
                         <path
                             d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L9 3.414V19a1 1 0 0 0 2 0V3.414l7.293 7.293a1 1 0 0 0 1.414-1.414Z" />
                     </svg>
-                    Dashboard
+                    Admin Panel
                 </a>
             </li>
             <li>
@@ -27,9 +27,9 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="m1 9 4-4-4-4" />
                     </svg>
-                    <a href="{{ route('allcourse') }}"
-                        class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 transition-colors">Kelola
-                        Kursus</a>
+                    <a href="{{ route('managecourses') }}"
+                        class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 transition-colors">Manage
+                        Courses</a>
                 </div>
             </li>
             <li>
@@ -137,7 +137,7 @@
                                     @if ($isEditMode)
                                         <button wire:click="updateLesson"
                                             class="px-6 py-2 bg-blue-600 text-white text-sm font-bold rounded shadow hover:bg-blue-700 transition-all">
-                                            Update  
+                                            Update
                                         </button>
                                     @else
                                         <button wire:click="saveLesson"
