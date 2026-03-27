b936c557a37f215138edd20ba4011003295727c4
Lenovo <orvinoray@gmail.com>
2026-03-27 23:26:51 +0700

    perbaikan model dari Enrollments

--- app/Models/Enrollments.php
+++ app/Models/Enrollments.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Enrollments extends Model
 {
-    //
+    protected $fillable = ['user_id', 'course_id', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Courses::class, 'course_id');
+    }
 }
